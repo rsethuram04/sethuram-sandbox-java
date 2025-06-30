a503e3ff2356d4aa1e3f8641ff0f0b2cdbfa15c3
sethuram1004 <sethuram.ramasamy@anywhere.co>
2025-06-30 11:14:13 +0530

    feat: spring boot skeleton

---
+++ sanbox/src/main/java/com/sethuram/sanbox/SethuramSandbox1Application.java
@@ -0,0 +1,13 @@
+package com.sethuram.sanbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SethuramSandbox1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SethuramSandbox1Application.class, args);
+	}
+
+}
---
+++ sanbox/src/test/java/com/sethuram/sanbox/SethuramSandbox1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sethuram.sanbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SethuramSandbox1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
